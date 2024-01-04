49ec37e57362db7ed25a31419b41e8db3a09dfac
HJavid010 <hoseinjavid138311@gmail.com>
2024-01-04 15:19:03 +0330

    date functions needed got added to header file.

--- src/date.hpp
+++ src/date.hpp
@@ -1,9 +1,22 @@
 #ifndef DATE_H
 #define DATE_H 1
 
+#include <iostream>
+
 struct date
 {
     unsigned int year, month, day;
+    int IsNotVaild();
+    int Equal(struct date);
+};
+
+struct weekday
+{
+    int number;
+    std::string ToGeorgian();
+    std::string ToShamsi();
+    std::string ToGamari();
+    date FirstRecurrence(date);
 };
 
 #endif
