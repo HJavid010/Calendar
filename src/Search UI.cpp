7847a7714e25eccf48cd1157dbf427486b666746
Arshiajh8 <arshia.jh8@gmail.com>
2024-01-05 22:51:01 +0330

    Ui prototype 0.2

---
+++ src/Search UI.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{int controller =0;
+    
+    cout << endl << "| help: 1.Search 2.tommorow's task 3.All at Once  4.Back |" << endl;
+        cout << "                Searchbar                       "<< endl;
+        cout << "Enter Date(YYYY-MM-DD):";
+        cin >> controller ;
+    
+    cin >> controller ;
+    while (controller==4)
+    {
+        break;
+    }
+    
+    return 0;
+}
